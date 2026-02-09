fa9dbba1f4d6be369b76097d562e2a8aa62ef8de
Debjit <debjit2k15@gmail.com>
2026-02-09 16:46:43 +0530

    2nd commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express=require("express");
 const app= express();
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send("Bolloooo from the server!")});
+app.get("/user",(req,res,next)=>{
+next();
+// res.send("A!")
+});
+app.get("/user",(req,res,next)=>{
+    // res.send("B!")
+    next();})
+app.get("/user",(req,res,next)=>{
+    res.send("C!")
+    next();})
+app.get("/user",(req,res,next)=>{
+    res.send("D!")
+    next();})
+app.get("/user",(req,res)=>{
+    res.send("E!")
+})
 
 app.listen(50000,()=>{console.log("Server is successfully listening")})
