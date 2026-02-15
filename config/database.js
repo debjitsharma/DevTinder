12c90dab0c5a2d519b71c3866baf020912763c13
Debjit <debjit2k15@gmail.com>
2026-02-15 10:44:20 +0530

    signup api

--- config/database.js
+++ config/database.js
@@ -3,13 +3,4 @@ const connectDB= async()=>{
     await mongoose.connect("mongodb+srv://debjitsharma:Iammongo1%23@nodedaev.w258dbs.mongodb.net/?appName=nodedaev");
 
 };
-connectDB()
-.then(()=>
-{
-    console.log("Database connection established....");
-})
-.catch(
-    (err)=>{
-        console.error("Database cannot be connected!!")
-    }
-);
+module.exports=connectDB;
--- src/app.js
+++ src/app.js
@@ -1,4 +1,32 @@
 const express=require("express");
-require("../config/database")
+const connectDB=require("../config/database")
 const app=express();
-app.listen(50000,()=>{console.log("Server is successfully listening")})
+const User=require("../models/user");
+app.post("/signup",async(req,res)=>{
+    const user=new User(
+        {
+            firstName:"Debjit",
+            lastName:"Sharma",
+            emailId:"debjit2k15@gmail.com",
+            password:"sjbdsjfb",
+        }
+    );
+    try{await user.save();
+res.send("User Added successfully!!");
+}catch(err){
+    res.status(400).send("error saving the user"+ err.message)
+}
+
+});
+
+connectDB()
+.then(()=>
+{
+    console.log("Database connection established....");
+    app.listen(50000,()=>{console.log("Server is successfully listening")})
+})
+.catch(
+    (err)=>{
+        console.error("Database cannot be connected!!")
+    }
+);
