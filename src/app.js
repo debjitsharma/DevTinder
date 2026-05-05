bc478005da282ec6e3e5205b67c97105280ae095
debjitsharma <debjit2k15@gmail.com>
2026-05-05 22:13:04 +0530

    socket

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const {adminAuth}= require("../middlewares/auth")
 const app=express();
 const cors=require("cors");
 const User=require("../models/user");
+const http= require("http"); 
 app.use(cors({
      origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials:true
@@ -24,6 +25,9 @@ app.use("/",profileRouter);
 app.use("/",connectionRequestRouter);
 app.use("/",userRouter);
 
+const server= http.createServer(app);
+const socket= require("socket.io");
+const io= socket(server, {cors})
 // app.post("/sendConnectionRequest",adminAuth, async(req,res)=>{
 //     const user=req.user;
 //     res.send(user.firstName + "sent the connection request")
@@ -32,7 +36,7 @@ connectDB()
 .then(()=>
 {
     console.log("Database connection established....");
-    app.listen(50000,()=>{console.log("Server is successfully listening")})
+    server.listen(50000,()=>{console.log("Server is successfully listening")})
 })
 .catch(
     (err)=>{
