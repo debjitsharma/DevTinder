f590b4342773c35d520a1f6445449eb44422f8b9
debjitsharma <debjit2k15@gmail.com>
2026-05-10 19:11:01 +0530

    updated

--- utils/socket.js
+++ utils/socket.js
@@ -8,7 +8,7 @@ io.on("connection",(socket)=>{
     socket.on("joinChat",({userId,targetUserId})=>{
     const roomId=[userId,targetUserId].sort().join('_');
     socket.join(roomId);
-    console.log(`${userId}joined room:{roomId}`);
+    console.log(`${userId}joined room:${roomId}`);
     });
     socket.on("sendMessage",()=>{})
     socket.on("disconnect",()=>{});
